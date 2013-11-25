d5979a5503c54a2c506ee01e5ba416cae8c69abb
cocu <cocu.436f6375@gmail.com>
2013-11-25 15:00:44 +0900

    hogera

--- demo-web/server.py
+++ demo-web/server.py
@@ -4,8 +4,12 @@ import tornado.websocket
 
 from tornado.options import define, options, parse_command_line
 
+import os
+import json
+
 define('port', default=8888, help='run on the given port', type=int)
 
+here = os.path.abspath(os.path.dirname(__file__))
 
 clients = dict()
 
@@ -13,7 +17,12 @@ clients = dict()
 class IndexHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self, *args, **kwargs):
-        self.write("this is your response")
+        self.set_header('Content-Type','application/json')
+        self.set_header('Access-Control-Allow-Origin','*')
+        
+        cnf_files = os.listdir(here+'/../sample_cnf')
+        res = json.dumps(cnf_files)
+        self.write(res)
         self.finish()
         
 
@@ -27,11 +36,17 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
     
     def on_message(self, message):
         print("Client %s received a message: %s" % (self.id, message))
-        if message == 'start':
+        l = message.split(' ')
+        cnf_files = os.listdir(here+'/../sample_cnf')
+        filename = 'input.cnf'
+        if len(l)>=2:
+            if l[1] in cnf_files:
+                filename = l[1]
+        if len(l)>0 and l[0]=='start':
             from subprocess import Popen, PIPE, STDOUT
             if self.p:
                 self.p.kill()
-            self.p = Popen(['python', '-OO', '../pysat/pysat-extended.py' ,'--sleep','100','--output-type','json', '../sample_cnf/input.cnf'], stdout=PIPE,stderr=STDOUT)
+            self.p = Popen(['python', '-OO', '../pysat/pysat-extended.py' ,'--sleep','100','--output-type','json', '../sample_cnf/'+filename], stdout=PIPE,stderr=STDOUT)
             for line in iter(self.p.stdout.readline, b''):
                 print(line)
                 self.write_message(line)
@@ -46,7 +61,7 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
 app = tornado.web.Application([
     (r'/', WebSocketHandler),
-    (r'/start', IndexHandler)
+    (r'/filename', IndexHandler),
 ])
 
 
