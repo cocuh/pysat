dbcd68c77e3010a074ba35f33b978c030c44d4a3
cocu <cocu.436f6375@gmail.com>
2013-11-28 12:33:27 +0900

    add enhance

--- demo-web/static/enhanced.js
+++ demo-web/static/enhanced.js
@@ -1,11 +1,42 @@
 window.onload = function () {
     var ws = null;
-    function start() {
+
+    function _start() {
         if (ws === null) {
             connect();
         }
         console.log('start');
         ws.send("start " + 'sudoku.cnf' + ' ' + 100 + ' ' + 1);
+    }
+
+    function start() {
+        var s = Snap("#svg");
+        var bigCircle = s.circle(150, 150, 100)
+        bigCircle.attr({
+            fill: "#bada55",
+            stroke: "#000",
+            strokeWidth: 5
+        });
+        var smallCircle = s.circle(100, 150, 70)
+        var discs = s.group(smallCircle, s.circle(200, 150, 70));
+        discs.attr({
+            fill: "#fff"
+        })
+        bigCircle.attr({
+            mask: discs
+        })
+        smallCircle.animate({r: 50}, 1000)
+        discs.select("circle:nth-child(2)").animate({r: 50}, 1000)
+        var p = s.path("M10-5-10,15M15,0,0,15M0-5-20,15").attr({
+            fill: "none",
+            stroke: "#bada55",
+            strokeWidth: 5
+        });
+        p=p.pattern(0,0,10,10)
+        bigCircle.attr({fill:p});
+        discs.attr({fill:"r()#fff-#000"})
+        discs.attr({fill:"R(150, 150, 100)#fff-#000"})
+p.select("path").animate({stroke:"#f00"},500)
     }
 
     function connect() {
@@ -26,5 +57,6 @@ window.onload = function () {
             elem_message.innerHTML = string;
         };
     }
+
     document.getElementById('start').onclick = start;
 };
